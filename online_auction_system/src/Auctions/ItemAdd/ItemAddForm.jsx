b387b86b7f8e3a85a495b45ff755ad0e33067208
anusari <116504727+anusari@users.noreply.github.com>
2024-05-07 15:52:05 +0530

    node modules

--- online_auction_system/src/Auctions/ItemAdd/ItemAddForm.jsx
+++ online_auction_system/src/Auctions/ItemAdd/ItemAddForm.jsx
@@ -1,8 +1,11 @@
 import { Link, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap. css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from '../../NavBar/Navbar';
+
 export default function ItemAddForm() {
+    const navigate = useNavigate();
 
 
 const [item, setItem] = useState({
@@ -15,6 +18,17 @@ const [item, setItem] = useState({
     imageData:""
 
   });
+
+//   const onInputChange = (e) => {
+//     if (e.target.name === 'imageData') {
+//       // For file inputs, use e.target.files to get the selected file
+//       setItem({ ...item, [e.target.name]: e.target.files[0] });
+//     } else {
+//       // For other inputs, set the value directly
+//       setItem({ ...item, [e.target.name]: e.target.value });
+//     }
+//   };
+  
 
 const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData} =item;
 
@@ -32,17 +46,20 @@ const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData
     }
     
     await axios.post("http://localhost:8080/additem", item);
-    // navigate("/manageitem");
+    navigate("/Auctions");
   };
 
 
   return (
     <div className='container-fluid'>
-        <div className=' justify-content-center'>
-            <div className='col-md-12 border rounded p-4 mt-2 shadow' style={{ maxHeight: '80vh', overflowY: 'auto', maxWidth: '1000px' }}>
+         <Navbar />
+         <div className='d-flex align-items-center justify-content-center' style={{ minHeight: '90vh' }}>
+            <div className='col-md-5 border rounded p-4 mt-1 shadow' style={{ maxHeight: '110vh', maxWidth: '500px' }}>
+            <h2 className='text-center m-4'>Add New Item</h2>
+
                 <form onSubmit={(e) => onSubmit(e)}>
                 <div className='col'>
-                <label htmlFor="itemname" className="form-label">itemname</label>
+                <label htmlFor="itemname" className="form-label">Item Name</label>
                 <input type="text" className='form-control'
                         placeholder='Enter itemname'
                         name="itemname"
@@ -51,7 +68,7 @@ const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData
                 />
                 </div>
                 <div className='col'>
-                <label htmlFor="description" className="form-label">description</label>
+                <label htmlFor="description" className="form-label">Description</label>
                 <input type="text" className='form-control'
                         placeholder='Enter description'
                         name="description"
@@ -60,7 +77,7 @@ const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData
                 />
                 </div>
                 <div className='col'>
-                <label htmlFor="startingPrice" className="form-label">startingPrice</label>
+                <label htmlFor="startingPrice" className="form-label">Starting Price</label>
                 <input type="text" className='form-control'
                         placeholder='Enter startingPrice'
                         name="startingPrice"
@@ -69,7 +86,7 @@ const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData
                 />
                 </div>
                 <div className='col'>
-                <label htmlFor="currentBid" className="form-label">currentBid</label>
+                <label htmlFor="currentBid" className="form-label">Current Bid</label>
                 <input type="text" className='form-control'
                         placeholder='Enter currentBid'
                         name="currentBid"
@@ -78,8 +95,8 @@ const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData
                 />
                 </div>
                 <div className='col'>
-                <label htmlFor="bidEndTime" className="form-label">bidEndTime</label>
-                <input type="text" className='form-control'
+                <label htmlFor="bidEndTime" className="form-label">BidEnd Time</label>
+                <input type="time" className='form-control'
                         placeholder='Enter bidEndTime'
                         name="bidEndTime"
                         value={bidEndTime}
@@ -87,14 +104,13 @@ const {itemid,itemname,description,startingPrice,currentBid,bidEndTime,imageData
                 />
                 </div>
                 <div className='col'>
-                <label htmlFor="imageData" className="form-label">imageData</label>
-                <input type="text" className='form-control'
-                        placeholder='Enter imageData'
+                <label htmlFor="imageData" className="form-label">Image Data</label>
+                <input type="file" className='form-control-file'
                         name="imageData"
-                        value={imageData}
                         onChange={(e) => onInputChange(e)}
                 />
                 </div>
+                <button type="submit" className='btn btn-outline-primary'>Submit</button>
                 </form>
             </div>
         </div>
